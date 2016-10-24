3b9f872c83bb9cac01cc08fd8c6c6ed112310903
Yaron Cohen <yaroncnk@gmail.com>
2016-10-24 22:49:05 +0000

    5 latest posts

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 var express = require('express');
+// load the mysql library
+var mysql = require('mysql');
+
+// create a connection to our Cloud9 server
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'yaroncnk', // CHANGE THIS :)
+    password: '',
+    database: 'reddit'
+});
+
+// load our API and pass it the connection
+var reddit = require('./reddit');
+var redditAPI = reddit(connection);
 
 var myWebServer = express();
 
@@ -36,16 +50,16 @@ myWebServer.get('/hello/:any_key_name', function(request, response) {
 //ex. 3 - this function performs numerical operations on two numbers
 myWebServer.get('/calculator/:operation', function(request, response) {
     var calculationProcess = {
-            operator: request.params.operation,
-            firstOperand: request.query.num1,
-            secondOperand: request.query.num2,
-    
-        };
+        operator: request.params.operation,
+        firstOperand: request.query.num1,
+        secondOperand: request.query.num2,
+
+    };
     if (request.params.operation === 'add') {
-        
-        
+
+
         calculationProcess.solution = parseInt(request.query.num1) + parseInt(request.query.num2);
-        
+
     }
     else if (request.params.operation === 'sub') {
         calculationProcess.solution = parseInt(request.query.num1) - parseInt(request.query.num2);
@@ -65,6 +79,42 @@ myWebServer.get('/calculator/:operation', function(request, response) {
 
     response.send(calculationProcess);
 
+});
+
+// ex. 4 - getAllPosts
+myWebServer.get('/posts', function(request, response) {
+
+    redditAPI.getAllPosts(5, function(err, posts) {
+        if (err) {
+            return err;
+        }
+        else {
+            var content = `<div id="contents">
+                            <h1>List of contents - five latest posts</h1>`;
+            posts.forEach(function(details) {
+                content +=
+                         `
+                             <ul class="contents-list">
+                             <li class="content-item">
+                                 <h2 class="content-item__title">
+                                  <a href=` + details.url + `> ` + details.title  +`</a>
+                                 </h2>
+                            <p>Created by ` + details.user.username + `</p>
+                 </li>
+  </ul>
+</div>`
+
+
+                //   `<h1>title:` + details.title + `</h1>
+                //   <p>url: ` + details.url + `</p>
+                //   <p>user: ` + details.user.username + `</p>`
+
+            })
+            response.send(content);
+        }
+
+
+    })
 });
 
 // This function will make the server listen 24/7 for incoming requests. Web servers never sleep!
